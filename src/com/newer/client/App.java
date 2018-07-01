4c3f1b213448d7b5252df8729d87ab85b5d4f587
bearGD <1113430218@qq.com>
2018-07-01 23:59:53 +0800

    实现客户端文件的上传和服务端文件的接收并存储到本地

--- src/com/newer/client/App.java
+++ src/com/newer/client/App.java
@@ -1,5 +1,12 @@
 package com.newer.client;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 /**
  * 客户端
  * 
@@ -7,7 +14,79 @@ package com.newer.client;
  *
  */
 public class App {
+
+	/**
+	 * 套接字
+	 */
+	Socket socket;
+	/**
+	 * 读取文件内容
+	 */
+	FileInputStream in;
+	/**
+	 * 端口号
+	 */
+	int serverPort = 9000;
+	/**
+	 * 服务器地址
+	 */
+	String serverAddress = "";
+	/**
+	 * 输出内容
+	 */
+	OutputStream out;
+
+	public void start() {
+		// 获取需要上传的文件
+		Scanner sc = new Scanner(System.in);
+		System.out.println("请输入需要上传的文件:");
+		String file = sc.next();
+		// 用完扫描器,关闭
+		sc.close();
+
+		// 建立链接
+
+		try {
+			// 实例化套接字
+			socket = new Socket(serverAddress, serverPort);
+			out = socket.getOutputStream();
+			in = new FileInputStream(file);
+			// 缓存buf,4kB
+			byte[] buf = new byte[1024 * 4];
+			int size = 0;
+			while (-1 != (size = in.read(buf))) {
+				out.write(buf, 0, size);
+				// 刷新
+				out.flush();
+			}
+			System.out.println("上传成功!");
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			// 关闭资源
+			try {
+				in.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			try {
+				out.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			try {
+				socket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
 	public static void main(String[] args) {
-		
+		App client = new App();
+		client.start();
 	}
 }
--- src/com/newer/fileserver/App.java
+++ src/com/newer/fileserver/App.java
@@ -1,5 +1,19 @@
 ﻿package com.newer.fileserver;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigInteger;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * 服务端
  * 
@@ -7,5 +21,90 @@
  *
  */
 public class App {
+	/**
+	 * 端口
+	 */
+	int port = 9000;
+	/**
+	 * 服务器套接字
+	 */
+	ServerSocket serverSocket;
+	/**
+	 * 线程池
+	 */
+	ExecutorService pool;
+
+	/**
+	 * 接收到文件存放于本地的地址
+	 */
+	String filePath = "";
+
+	public void start() {
+
+		// 获取存放文件的本地地址
+		Scanner sc = new Scanner(System.in);
+		System.out.println("请输入您要存放文件的本地地址:");
+		filePath = sc.next();
+		sc.close();
+
+		try {
+			serverSocket = new ServerSocket(port);
+
+			while (true) {
+				// 获取套接字
+				Socket socket = serverSocket.accept();
+				pool = Executors.newCachedThreadPool();
+
+				pool.execute(new Runnable() {
+
+					// 缓存,可变数组
+					ByteArrayOutputStream data = new ByteArrayOutputStream();
+
+					@Override
+					public void run() {
+						try (InputStream in = socket.getInputStream()) {
+							byte[] buf = new byte[1024 * 4];
+							int size = 0;
+							while (-1 != (size = in.read(buf))) {
+								// 数据写入可变数组缓存
+								data.write(buf, 0, size);
+							}
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+
+						// 接收到的数据,字节数组存放
+						byte[] info = data.toByteArray();
+						String file = "";
+
+						try {
+							byte[] hash = MessageDigest.getInstance("SHA-256").digest(info);
+							file = new BigInteger(1, hash).toString(16);
+						} catch (NoSuchAlgorithmException e) {
+							e.printStackTrace();
+						}
+
+						try (FileOutputStream out = new FileOutputStream(new File(filePath,file))) {
+							out.write(info);
+							System.out.println("上传完成!");
+						} catch (Exception e) {
+							System.out.println("上传失败!");
+						}
+
+					}
+
+				});
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		App server = new App();
+		server.start();
+	}
 
 }
